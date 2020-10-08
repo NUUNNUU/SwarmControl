d4ee9b31327fe13e63ecc62f60490f5c77f8589c
deepdaehyeon <64945866+deepdaehyeon@users.noreply.github.com>
2020-10-08 18:49:48 +0900

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,43 +1,64 @@
 import numpy as np 
 
+
+class Environment():
+    def __init__(self, mapSize, laserSize):
+        self.mapSize = mapSize 
+        self.laserSize = laserSize 
+        self.objects = list() 
+        # object의 형식은 [kind, p_center, size, angle]
+        # kind = [0:"laser", 1:"robot", 2:"obstacle"]
+        # p_center = ["laser","robot": center point, , "obstacle": 꼭지점1]
+        # p_size = ["laser","robot": size, , "obstacle": 꼭지점2]
+        # angle = ["laser", "obstacle":0 ,"robot": angle[0~pi)]
+                
+    def add_obs(self, p1, p2):
+        # p1,p2는 square block의 꼭지점 
+        # obs = [kind, p_center, size, angle]
+        self.objects.append(obs)
+        
+    def add_bot(self, p_init, n_bot, ranges=2, size=250 ):
+        # initial point 근처에 랜덤하게 로봇 생성
+        centers  = np.random.randint(p_init,p_init+ranges, size=(2,n_bot))
+        angles = np.random.randint(np.pi, size=(n_bot))
+        for c,a in (centers, angles):
+            self.objects.append([1,c,size,a])
+        
+    def move_laser(self, p_start, p_end):
+        tic = 1 # laser scan speed
+        vel = np.subtract(p_end,p_start)
+        self.vel = np.mul(vel)*(tic/np.norm(vel)) 
+        p_laser = p_start
+        while True:
+            if collide_check_lb(p_laser):                
+                push_lb()
+                if collide_check_bb():
+                    push_bb()                    
+            p_laser +=vel
+                
+    def push_lb(self):
+    def push_bb(self):
+    def collide_check_lb(self, p_laser):
+    def collide_check_bb(self):
+    def collide_check_ob(self):
+        
+
+    
+    
+# def collide_check_lb(): 
+#     flag = False
+#     p1,p2 = p[0], p[1]    
+#     v =  np.subtract(p1,p2)
+#     v_p1 = np.subtract(c,p1)
+#     v_p2 = np.subtract(c,p2)
+#     if min(dist(p1,c), dist(p2,c)) <r:
+#         flag = True         
+#     if np.linalg.norm(np.dot(v_p1,v))<r
+#     and np.dot(v_p1, v_p2)<0:
+#         flag = True 
+#     return flag 
+
 def dist(p1,p2):
     return np.linalg.norm(np.subtract(p1,p2))
 
-def collide_laser_bot(p,c,r):
-    flag = False
-    p1,p2 = p[0], p[1]    
-    v =  np.subtract(p1,p2)
-    v_p1 = np.subtract(c,p1)
-    v_p2 = np.subtract(c,p2)
-    if min(dist(p1,c), dist(p2,c)) <r:
-        flag = True         
-    if np.linalg.norm(np.dot(v_p1,v))<r
-    and np.dot(v_p1, v_p2)<0:
-        flag = True 
-    return flag 
-
-class Environment():
-    def __init__(self, mapSize):
-        self.mapsize = mapsize 
-        
-    def add_obs(self, p1, p2):
-        
-    def add_bot(self, p_swarm, n_bot):
-        
-        
-        
-        
-        
-
-if __name__ =="__main__":
-    # Parameter setting 
-    mapSize =(1920,1200) 
-    n_bot = 100 
-    n_sampling = 20
-    p_init = (10,10)
-    p_goal = (50,1000)    
-    p_swarm = np.random.randint(30, size=(2,n_bot))
-    
-    # Create simulation environment 
-    envs = Environment(mapSize) # func create_envs including func create_obstacle 
     
