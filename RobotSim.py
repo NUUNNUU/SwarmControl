f22fc44c688880c1ab1971d67e441d5734411fac
deepdaehyeon <64945866+deepdaehyeon@users.noreply.github.com>
2020-10-12 14:56:44 +0900

    Update RobotSim.py
    코멘트 남긴 부분 수정이랑 물리 엔진 구현만하면 될 것 같아요~

--- RobotSim.py
+++ RobotSim.py
@@ -1,22 +1,29 @@
+import pygame
+
 import math
 import random
 import pygame
 import time
 
+# Global parameters 
 winSize = (1080,720)
-
-colorBack  = (100,100,100)
+colorBack  = (180,180,180)
 colorRobot = (255,255,255)
 colorLaser = (255,0,0)
 colorWall  = (50,50,50)
 
+radLaser = 5 
+fric = 1 
+bounce = 1 
+bbounce = 1 
+
 ####################################################################
 class Robot():
-    def __init__(self, pos, vec, len):
+    def __init__(self, pos, vec, leng):
         self.position = pos
         self.vector = vec
-        self.length = len
-    #
+        self.leng = leng 
+    
     def posture(self):
         center = self.position
         vector = unitVec(self.vector)
@@ -28,55 +35,70 @@ class Robot():
         #pygame.draw.circle(win, colorRobot, self.position, 2,2)
         pygame.draw.line(win, colorRobot, self.posture()[1], self.posture()[2], 10)
         pygame.display.update()
-    #
-#
+        
+class Laser():
+    def __init__(self, pos): 
+        self.position = pos
+    
+    def draw(self, win):
+        pygame.draw.circle(win, colorLaser, self.position, radLaser, radLaser-1)
+        pygame.display.update()
+        
+        
 def drawField(win):
     win.fill(colorBack)
-    pygame.draw.line(win, colorWall, (10,10), (10,510), 10)
+    pygame.draw.line(win, colorWall, (10,10), (10,10), 10)
     pygame.draw.line(win, colorWall, (10,510), (510,510), 10)
     pygame.draw.line(win, colorWall, (510,510), (510,10), 10)
     pygame.draw.line(win, colorWall, (510,10), (10,10), 10)
-#
-def randomBots(range, n): # randomly set n number of robots in the field
-    #for i in range(n):
+    # obstacle wall 추가 필요
+    # map 크기 확장 필요
+    
+def randomBots(range, n=30,center=(10,10),sigma=1): # randomly set n number of robots in the field 
+    # center를 중심으로 sigma 정규분포를 따르는 n개의 로봇 생성    
     pass
-#
-def unitVec(vec):
+
+def normalize(vec):
     mag = math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
     return (vec[0]/mag, vec[1]/mag)
 
+def physics():
+    pass 
 
-#Rob = Robot((1,1), (3,1), 3)
-
-####################################################################
-
-if __name__ == "__main__":
-    botList = []
+if __name__ == "__main__":    
+    # initialize environment
     clock = pygame.time.Clock()
-
     pygame.init()
     pygame.display.set_caption('Sim')
     win = pygame.display.set_mode(winSize)
-    win.fill(colorBack)
+    win.fill(colorBack) 
     
-    run = True
-    while run:
-        pygame.time.delay(30)
-        drawField(win)
-
-        for bot in botList:
-            bot.draw(win)
-        
-        pygame.display.update()
-    
+    # intialize bot 
+    bots =[]    
+    # bots = randomBots()
+            
+    ###############
+    ## main loop ##
+    ###############
+    while True:      
+        drawField(win) 
+        for bot in bots:
+            bot.draw(win)  
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: # terminate
-                run = False
-                
-            elif event.type == pygame.MOUSEBUTTONDOWN: # add a robot
+            if event.type == pygame.KEYDOWN:
+                # Terminate game
+                if event.key == pygame.K_ESCAPE:  
+                    pygame.quit()      
+                # Control laser 
+                if event.key == pygame.K_LCTRL: 
+                    Laser(pygame.mouse.get_pos()).draw(win)
+                    
+            # Add a robot
+            elif event.type == pygame.MOUSEBUTTONDOWN: 
                 x,y = pygame.mouse.get_pos()
-                botList= [Robot((x,y), (2,3),0)]
-                #botList.append(Robot((x,y), 0,0))
+                bots.append(Robot((x,y),(2,3),0))
                 
-        #pygame.display.update()
-
+        #physics() # 물리엔진 구현필요     
+        pygame.display.flip() # update와 같은 기능 
+        clock.tick(30) 
+        time.sleep(0.03)
